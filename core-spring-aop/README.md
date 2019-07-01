1cf58cc6c89f17ab0c6bdf7afdc80337a15d51ba
颛孙鹏程 <zhuansunpengcheng@qq.com>
2019-07-01 15:23:58 +0800

    update : spring-aop源码分析

---
+++ core-spring-aop/README.md
@@ -0,0 +1,5 @@
+# core-spring-aop
+
+spring-aop的源码解析
+
+
---
+++ core-spring-aop/src/test/com/zpsc/core/spring/aop/test/AOPTest.java
@@ -0,0 +1,22 @@
+package com.zpsc.core.spring.aop.test;
+
+import com.zspc.core.spring.aop.config.MainConfig;
+import com.zspc.core.spring.aop.service.Calculator;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * @author zhuansunpengcheng
+ * @create 2019-06-27 2:53 PM
+ **/
+
+public class AOPTest {
+    @Test
+    public void testAop() {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(MainConfig.class);
+        Calculator calculator = (Calculator) applicationContext.getBean("calculator");
+        int div = calculator.div(2, 1);
+        System.out.println(div);
+    }
+}
