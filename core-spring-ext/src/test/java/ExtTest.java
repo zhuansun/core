c606116e2098b7ac935e0961d39ae0c651146121
颛孙鹏程 <zhuansunpengcheng@qq.com>
2019-07-31 16:14:47 +0800

    add

--- core-spring-ext/src/test/java/ExtTest.java
+++ core-spring-ext/src/test/java/ExtTest.java
@@ -1,8 +1,11 @@
 import com.zspc.core.spring.ext.MainConfig;
 import org.junit.Test;
-import org.springframework.context.ApplicationEvent;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.event.ContextClosedEvent;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author zhuansunpengcheng
@@ -19,5 +22,28 @@ public class ExtTest {
         applicationContext.close();
     }
 
+    @Test
+    public void test2(){
+
+        String a1 = "123,45,23";
+
+
+        String b1 = "123,45,12";
+
+
+        System.out.println(compareBeforeAndCurrent(a1,b1));
+
+
+    }
+
+
+    private String compareBeforeAndCurrent(String before, String current){
+        List<String> be = new ArrayList<>();
+        be.addAll(Arrays.asList(before.split(",")));
+        List<String> cu =  new ArrayList<>();
+        cu.addAll(Arrays.asList(current.split(",")));
+        be.removeAll(cu);
+        return be.toString();
+    }
 
 }
